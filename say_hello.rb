a1d4e059e168d11241ff1a8c5ad7de70a9679f77
Anthony Molina <antonio-molina@att.net>
2019-05-17 04:59:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,3 @@
-def greeting (name)
-  puts "Hello, #{Anthony}"
+def greeting (name="Ruby programmer", language="Ruby")
+  puts "Hello, #{Anthony}. we heard you are a great #{language} programmer."
 end
