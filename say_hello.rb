d3cae0bef9d1c7de8dbc98b954c6d8c515591213
Anthony Molina <antonio-molina@att.net>
2019-05-17 05:28:26 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,3 @@
    #Method name    first_argument, second_argument
    def greeting_programmer(name, lanuage)
-     puts "Hello, {Anthony}. we heard you are a great# {Ruby} programmer."
-   end
+     
